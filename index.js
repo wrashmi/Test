7bf523fc8f21ef8ce3e39131410c3a2426e49e9d
Shafeen M <sodl02@gmail.com>
2016-05-06 11:19:48 -0400

    updated gitignore and updated index.js

--- index.js
+++ index.js
@@ -2,7 +2,19 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World!');1
+});
+
+app.get('/shafeen', function (req, res) {
+  res.send('<b>Hello Shafeen!</b>');
+});
+
+app.get('/budget/monthly/', function (req, res) {
+  res.send('<b>Hello Rashmi!</b>');
+});
+
+app.get('/our/site/', function (req, res) {
+  res.sendfile('index.html');
 });
 
 app.listen(3000, function () {
